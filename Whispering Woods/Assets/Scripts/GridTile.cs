ffbd6a49a5dcc4cfaee6398b08ecff6abb00a76f
xanderwilson030 <54119525+xanderwilson030@users.noreply.github.com>
2024-01-30 11:36:49 -0500

    Working on Grid Based Movement

---
+++ Whispering Woods/Assets/Scripts/GridTile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTile : MonoBehaviour
+{
+    public Transform gridCenter;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Whispering Woods/Assets/Scripts/PlayerController.cs
+++ Whispering Woods/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,12 +10,21 @@ public class PlayerController : MonoBehaviour
     public int maxHp;
     public float moveSpeed;
 
+    [Header("Movement Details")]
+    [SerializeField] private Tilemap tm;
+    [SerializeField] private GridTile startingPos;
+    public LayerMask movementLayerMask;
+
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private Vector2 facingDirection;
 
     private void Start()
     {
+
+        Vector3Int cellPos = tm.WorldToCell(startingPos.gameObject.transform.position);
+        gameObject.transform.position = tm.GetCellCenterWorld(cellPos);
+
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
 
@@ -23,36 +33,51 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        Move();
+        TryMovement();
     }
 
-    private void Move()
+    private void TryMovement()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        RaycastHit2D hit;
 
-        Vector2 vel = new Vector2(x, y);
-
-        if (vel.magnitude != 0)
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            facingDirection = vel;
+            hit = Physics2D.Raycast((Vector2)transform.position + Vector2.up, Vector3.up, 1f, movementLayerMask);
+            Debug.DrawRay((Vector2)transform.position + Vector2.up, Vector3.up, Color.red, 1f);
+
+            MovePlayer(hit);
         }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            hit = Physics2D.Raycast((Vector2)transform.position + Vector2.down, Vector3.down, 1f, movementLayerMask);
+            Debug.DrawRay((Vector2)transform.position + Vector2.down, Vector3.down, Color.red, 1f);
 
-        UpdateSpriteDirection();
+            MovePlayer(hit);
 
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            hit = Physics2D.Raycast((Vector2)transform.position + Vector2.left, Vector3.left, 1f, movementLayerMask);
+            Debug.DrawRay((Vector2)transform.position + Vector2.left, Vector3.left, Color.red, 1f);
 
-        rb.velocity = vel * moveSpeed;
+            MovePlayer(hit);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            hit = Physics2D.Raycast((Vector2)transform.position + Vector2.right, Vector3.right, 1f, movementLayerMask);
+            Debug.DrawRay((Vector2)transform.position + Vector2.right, Vector3.right, Color.red, 1f);
+
+            MovePlayer(hit);
+        }
     }
 
-    private void UpdateSpriteDirection()
+    private void MovePlayer(RaycastHit2D hit)
     {
-        if (facingDirection == Vector2.right)
-        {
-            sr.flipX = false;
-        }
-        else if (facingDirection == Vector2.left)
-        {
-            sr.flipX = true;
-        }
+        if (hit.collider == null)
+            return;
+
+        gameObject.transform.position = hit.collider.gameObject.GetComponent<GridTile>().gridCenter.position;
+        //hit.collider.gameObject.SetActive(false);
     }
+
 }
